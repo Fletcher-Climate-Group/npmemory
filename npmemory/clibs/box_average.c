f891dd3c55806649c1855833bf86f9586cdfb6a6
Daniel Hogg <danielhogg@protonmail.com>
2020-09-09 09:40:34 -0400

    Started to work on C implementation

--- npmemory/clibs/box_average.c
+++ npmemory/clibs/box_average.c
@@ -24,3 +24,68 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // Sample code which takes an input n x m grid, and performs a box average.
 // Note: It is expected that n mod k = 0, and m mod j = 0.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+void crash_logger(error_code){
+    // If program encounters a problem, crash
+    // immediately to prevent memory corruption.
+
+    printf("npmemory encountered an error in C module.\n");
+    printf("Exiting with error code %d", error_code);
+    exit(error_code);
+}
+
+
+int work_size(int xdim, int ydim, int xstep, int ystep){
+    // Some bounds checking and modular arithmetic.
+
+    if (xdim % xstep != 0){
+        printf("Incompatible xstep specified: %d", xstep);
+        crash_logger(-1);
+    }
+
+    if (ydim % ystep != 0){
+        printf("Incompatible ystep specified: %d", ystep);
+        crash_logger(-1);
+    }
+
+    int xgrid = xdim / xstep;
+    int ygrid = ydim / ystep;
+
+    int work_grid_size = xgrid * ygrid;
+
+    return work_grid_size;
+}
+
+
+void box_average(void * np_ptr, int xdim, int ydim, int xstep, int ystep){
+    // Passing a void pointer, which is then cast into a 1D float
+    // array. Key thing to remember: All numpy ndarray objects are 1D
+    // in C (in terms of how they are physically represented).
+
+    // So, we have a 2D array in Python which is represented as a
+    // flattened 1D array in C.
+
+    float * box;
+    box = (float*) np_ptr;
+
+    // Array to store the intermediate results of the box average.
+    int work_array_size = work_size(xdim, ydim, xstep, ystep);
+    float * work_array = malloc(sizeof(float) * work_array_size);
+
+    int i, j;
+
+    for (i = 0; i < xdim; i++){
+        for (j = 0; i < ydim; j++){
+            int work_idx;
+            // Calculate work index
+
+        }
+    }
+
+    return;
+}
